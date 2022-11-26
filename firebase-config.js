d46b198512faabdd910047e969689ad9df8ea722
Akshin Vemana <avemana6@gatech.edu>
2022-11-26 12:56:00 -0500

    initial app commit

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi19gMzVtiJdPrHdoBKUyh3I0X2tTFHlo",
+  authDomain: "jdb-2315.firebaseapp.com",
+  projectId: "jdb-2315",
+  storageBucket: "jdb-2315.appspot.com",
+  messagingSenderId: "636234894329",
+  appId: "1:636234894329:web:02f63dbe119494506a0b28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db
